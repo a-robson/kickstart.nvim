52bbf341d84d55ad9f7294929ba0aaff5ac6fcf9
a-robson <andrew.d.robson@gmail.com>
2025-05-27 17:16:57 +0100

    modularise options

--- init.lua
+++ init.lua
@@ -65,6 +65,9 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
+-- [[ Setting options ]]
+require 'options'
+
 -- [[ Basic Keymaps ]]
 require 'keymaps'
 
